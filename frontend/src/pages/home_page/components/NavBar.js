e6234370f8d85595c6ef58ab8b3ab1964477c006
Mostafa <gmelkabany@gmail.com>
2020-08-16 22:15:56 +0200

    x

--- frontend/src/pages/home_page/components/NavBar.js
+++ frontend/src/pages/home_page/components/NavBar.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import './NavBar.css';
 
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+  
+
+{/* <Router>
+<Link to="product">Home</Link>
+</Router> */}
+
 export default class NavBar extends Component {
 
 
 
     render() {
+        <Router>
+        </Router>
+
+
         if (this.props.categories){
             this.links = [{id:0, name:'all'}].concat(this.props.categories).map((cat,ind)=>{
-                return <a onClick={this.props.handleClick} id={cat.id}  href="#">{cat.name}</a>
+                return (
+                        <Link path='product'>
+                            <a onClick={this.props.handleClick} id={cat.id}  href="#">{cat.name}</a>
+                        </Link>
+                )
                 
             });
-    }
+        }
     else {
         this.links = null;
     }
