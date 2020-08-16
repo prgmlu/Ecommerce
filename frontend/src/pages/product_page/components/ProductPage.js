ab0e6839a247792c69cb1f90499e9f3e3ae524e1
Mostafa <gmelkabany@gmail.com>
2020-08-16 20:59:37 +0200

    x

---
+++ frontend/src/pages/product_page/components/ProductPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class ProductPage extends Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    product page
+                </p>
+            </div>
+        )
+    }
+}
